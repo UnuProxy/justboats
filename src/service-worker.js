25f32f99fbb1bb60b58fdbc0787f68cec4d61e36
UnuProxy <129114953+UnuProxy@users.noreply.github.com>
2025-01-02 23:11:33 +0100

    Fixed service worker configuration and successful build

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,14 +1,22 @@
-// In the src/registerServiceWorker.js
-export function register() {
-    if ('serviceWorker' in navigator) {
-        window.addEventListener('load', () => {
-            navigator.serviceWorker.register('/src/service-worker.js')
-                .then(registration => {
-                    console.log('ServiceWorker registration successful with scope: ', registration.scope);
-                })
-                .catch(error => {
-                    console.error('ServiceWorker registration failed: ', error);
-                });
-        });
-    }
-   }
+/* eslint-disable no-restricted-globals */
+
+// This variable is required and should be first
+self.__WB_MANIFEST = []; 
+
+self.addEventListener('install', () => {
+  console.log('Service worker installed');
+});
+
+self.addEventListener('activate', () => {
+  console.log('Service worker activated');
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request)
+      .catch(() => {
+        // Handle offline functionality here if needed
+        return new Response('Offline');
+      })
+  );
+});
