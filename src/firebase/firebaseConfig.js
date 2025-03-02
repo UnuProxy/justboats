85861bbf3330cb353d107219046c6345ffaeed25
UnuProxy <129114953+UnuProxy@users.noreply.github.com>
2025-03-02 23:14:33 +0100

    erori

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -1,8 +1,12 @@
+
+
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getStorage } from "firebase/storage";
+
 import { getFunctions } from "firebase/functions";
+
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
@@ -15,15 +19,19 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
-const googleProvider = new GoogleAuthProvider(); 
+const googleProvider = new GoogleAuthProvider();
 const db = getFirestore(app);
 const storage = getStorage(app);
+
 
 const functions = getFunctions(app);
 functions.region = "us-central1";
 
 export { auth, googleProvider, db, storage, functions };
 export default app;
+
+
+
 
 
 
